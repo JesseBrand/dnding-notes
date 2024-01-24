779c0dbda1cf7fd56a54dc5e029a27f1698736af
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2024-01-24 16:26:11 +0100

    added linting config and made linter happy

--- components/MDContainer.js
+++ components/MDContainer.js
@@ -1,24 +1,30 @@
-import React, {useState} from 'react';
-import MDContent from "./MDContent";
-import {v4 as uuidv4} from 'uuid';
+import { useState } from 'react'
+import MDContent from './MDContent'
+import { PropTypes } from 'prop-types'
+import { v4 as uuidv4 } from 'uuid'
 
-function MDContainer({post, fileNames}) {
-    const [posts, setPosts] = useState([post]);
+function MDContainer ({ post, fileNames }) {
+  const [posts, setPosts] = useState([post])
 
-    function handleClick(content) {
-        // console.log(content)
-        setPosts(prevPosts => {
-            return [...prevPosts, content]
-        })
-    }
+  function handleClick (content) {
+    // console.log(content)
+    setPosts(prevPosts => {
+      return [...prevPosts, content]
+    })
+  }
 
-    return (
+  return (
         <div className="Container">
             {posts.map(p => (
                 <MDContent key={uuidv4()} content={p} handleOpenNewContent={handleClick} fileNames={fileNames}/>
             ))}
         </div>
-    );
+  )
 }
 
-export default MDContainer;
+MDContainer.propTypes = {
+  post: PropTypes.any,
+  fileNames: PropTypes.any
+}
+
+export default MDContainer
--- pages/index.js
+++ pages/index.js
@@ -1,55 +1,63 @@
-import Layout from "../components/Layout";
+import PropTypes from 'prop-types'
+import Layout from '../components/Layout'
 import {
-    getSinglePost,
-    getDirectoryData,
-    convertObject,
-    getFlattenArray,
-    getLocalGraphData,
-    constructGraphData
-} from "../lib/utils";
-import FolderTree from "../components/FolderTree";
+  getSinglePost,
+  getDirectoryData,
+  convertObject,
+  getFlattenArray,
+  getLocalGraphData,
+  constructGraphData
+} from '../lib/utils'
+import FolderTree from '../components/FolderTree'
 import dynamic from 'next/dynamic'
-import MDContent from "../components/MDContent";
-
+import MDContent from '../components/MDContent'
 
 // This trick is to dynamically load component that interact with window object (browser only)
 const DynamicGraph = dynamic(
-    () => import('../components/Graph'),
-    { loading: () => <p>Loading ...</p>, ssr: false }
+  () => import('../components/Graph'),
+  { loading: () => <p>Loading ...</p>, ssr: false }
 )
 
-export default function Home({graphData, content, tree, flattenNodes, backLinks}) {
-    return (
+export default function Home ({ graphData, content, tree, flattenNodes, backLinks }) {
+  return (
         <Layout>
             <div className = 'container'>
                 <nav className="nav-bar">
                     <FolderTree tree={tree} flattenNodes={flattenNodes}/>
                 </nav>
-                <MDContent content={content}  handleOpenNewContent={null} backLinks={backLinks}/>
+                <MDContent content={content} handleOpenNewContent={null} backLinks={backLinks}/>
                 <DynamicGraph graph={graphData}/>
             </div>
         </Layout>
-    );
-
+  )
 }
-const {nodes, edges} = constructGraphData()
 
-export function getStaticProps() {
-    const tree = convertObject(getDirectoryData());
-    const contentData = getSinglePost("index");
-    const flattenNodes = getFlattenArray(tree)
-    const listOfEdges =   edges.filter(anEdge => anEdge.target === "index")
-    const internalLinks = listOfEdges.map(anEdge => nodes.find(aNode => aNode.slug === anEdge.source)).filter(element => element !== undefined)
-    const backLinks = [...new Set(internalLinks)]
+Home.propTypes = {
+  content: PropTypes.any,
+  backLinks: PropTypes.any,
+  tree: PropTypes.any,
+  flattenNodes: PropTypes.any,
+  graphData: PropTypes.any
+}
 
-    const graphData = getLocalGraphData("index");
-    return {
-        props: {
-            content: contentData.data,
-            tree: tree,
-            flattenNodes: flattenNodes,
-            graphData:graphData,
-            backLinks: backLinks
-        },
-    };
+const { nodes, edges } = constructGraphData()
+
+export function getStaticProps () {
+  const tree = convertObject(getDirectoryData())
+  const contentData = getSinglePost('index')
+  const flattenNodes = getFlattenArray(tree)
+  const listOfEdges = edges.filter(anEdge => anEdge.target === 'index')
+  const internalLinks = listOfEdges.map(anEdge => nodes.find(aNode => aNode.slug === anEdge.source)).filter(element => element !== undefined)
+  const backLinks = [...new Set(internalLinks)]
+
+  const graphData = getLocalGraphData('index')
+  return {
+    props: {
+      content: contentData.data,
+      tree,
+      flattenNodes,
+      graphData,
+      backLinks
+    }
+  }
 }
